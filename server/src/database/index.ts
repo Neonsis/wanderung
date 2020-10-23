6d96e64b3352069ecefd81f9e79128823580d007
Andrey Vinel <vinel.work03@gmail.com>
2020-10-23 22:25:52 +0300

    Listing and Database types

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,10 +1,11 @@
 import {MongoClient} from "mongodb";
+import {Database, Listing} from "../types";
 
 const url = `mongodb+srv://${process.env.DB_USER}:${
     process.env.DB_USER_PASSWORD
 }@${process.env.DB_CLUSTER}.mongodb.net`;
 
-export const connectDatabase = async () => {
+export const connectDatabase = async (): Promise<Database> => {
     const client = await MongoClient.connect(url, {useNewUrlParser: true});
     const db = client.db("wanderung");
 
