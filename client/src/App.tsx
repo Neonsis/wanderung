c51311a48181e4df9a5e5648f9e10a6cdd691ae1
Andrey Vinel <vinel.work03@gmail.com>
2020-10-24 17:11:31 +0300

    Login UI Component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import {Home, Host, Listing, Listings, NotFound, User} from "./components";
+import {Home, Host, Listing, Listings, Login, NotFound, User} from "./components";
+import {Layout} from "antd";
 
 function App() {
     return (
         <Router>
-            <Switch>
-                <Route exact path="/" component={Home}/>
-                <Route exact path="/host" component={Host}/>
-                <Route exact path="/listing/:id" component={Listing}/>
-                <Route exact path="/listings/:location?" component={Listings}/>
-                <Route exact path="/user/:id" component={User}/>
-                <Route exact component={NotFound}/>
-            </Switch>
+            <Layout id="app">
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/host" component={Host}/>
+                    <Route exact path="/listing/:id" component={Listing}/>
+                    <Route exact path="/listings/:location?" component={Listings}/>
+                    <Route exact path="/login" component={Login}/>
+                    <Route exact path="/user/:id" component={User}/>
+                    <Route exact component={NotFound}/>
+                </Switch>
+            </Layout>
         </Router>
     );
 }
