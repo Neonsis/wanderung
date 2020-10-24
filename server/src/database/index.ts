b735790805f57556431ae4a03827573cb9e2df58
Andrey Vinel <vinel.work03@gmail.com>
2020-10-24 11:49:18 +0300

    Updated seed data

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -6,7 +6,9 @@ const url = `mongodb+srv://${process.env.DB_USER}:${
 }@${process.env.DB_CLUSTER}.mongodb.net`;
 
 export const connectDatabase = async (): Promise<Database> => {
-    const client = await MongoClient.connect(url, {useNewUrlParser: true});
+    const client = await MongoClient.connect(url, {
+        useNewUrlParser: true, useUnifiedTopology: true
+    });
     const db = client.db("wanderung");
 
     return {
