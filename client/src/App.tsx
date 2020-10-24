6c999c0746d87bc07e719492800006b541ce056b
Andrey Vinel <vinel.work03@gmail.com>
2020-10-24 10:51:24 +0300

    Base routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import {Listings} from "./components/Listings/";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {Home, Host, Listing, Listings, NotFound, User} from "./components";
 
 function App() {
     return (
-        <div>
-            <Listings/>
-        </div>
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route exact path="/host" component={Host}/>
+                <Route exact path="/listing/:id" component={Listing}/>
+                <Route exact path="/listings/:location?" component={Listings}/>
+                <Route exact path="/user/:id" component={User}/>
+                <Route exact component={NotFound}/>
+            </Switch>
+        </Router>
     );
 }
 
